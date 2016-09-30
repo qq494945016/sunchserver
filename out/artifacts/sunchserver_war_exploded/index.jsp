8e7a2b07babbbcf514186e8d89b7f5fd2d6d1b4b
qq494945016 <494945016@qq.com>
2016-09-30 17:18:40 +0800

    个人主页后台代码提交

---
+++ out/artifacts/sunchserver_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: huxs
+  Date: 16/9/30
+  Time: 上午11:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>个人主页后台</title>
+  </head>
+  <body>
+  个人主页后台已经开启
+  </body>
+</html>
---
+++ src/testSpring/test.java
@@ -0,0 +1,17 @@
+package testSpring;
+
+/**
+ * Created by huxs on 16/9/30.
+ */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.test.bean.Person;
+
+public class test {
+    public static void main(String[] args){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");//读取bean.xml中的内容
+        Person p = ctx.getBean("person",Person.class);//创建bean的引用对象
+        p.info();
+    }
+}
